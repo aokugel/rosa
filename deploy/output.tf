e19041a9fe1e068f746ebbb29e6a69d9aa67028c
Anthony Kugel <anthonykugel@gmail.com>
2024-04-04 07:59:32 -0500

    fixed outputs

--- deploy/output.tf
+++ deploy/output.tf
@@ -1,3 +1,24 @@
-output "openshift_url" {
-    value = rhcs_cluster_rosa_classic.rosa_sts_cluster.api_url
+output "api_url" {
+    #value = rhcs_cluster_rosa_classic.rosa_sts_cluster.api_url
+    value = join("", ["https://api.",rhcs_cluster_rosa_classic.rosa_sts_cluster.domain,":6443"])
 }
+
+output "console_url" {
+    #value = rhcs_cluster_rosa_classic.rosa_sts_cluster.console_url
+    value = join("", ["https://console-openshift-console.apps.",rhcs_cluster_rosa_classic.rosa_sts_cluster.domain])
+}
+
+output "domain" {
+    value = rhcs_cluster_rosa_classic.rosa_sts_cluster.domain
+}
+
+output "admin_username" {
+    value = rhcs_cluster_rosa_classic.rosa_sts_cluster.admin_credentials.username
+    sensitive = false
+}
+
+output "admin_password" {
+    value = rhcs_cluster_rosa_classic.rosa_sts_cluster.admin_credentials.password
+    sensitive = true
+}
+
